34ab6dfe969fb611d64d88bbafb41876b4e0a84a
Arthur Lima <mozart.bachh@gmail.com>
2022-11-16 01:10:54 +0100

    Criando as 2 novas telas do aplicativo: Login e registro através de e-mail.

---
+++ app/src/main/java/co/araujoarthur/instagramremake/register/view/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package co.araujoarthur.instagramremake.register.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import co.araujoarthur.instagramremake.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
