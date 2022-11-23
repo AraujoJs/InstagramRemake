f56756648d1802cb2f472cc41f114033506a7e5b
Arthur Lima <mozart.bachh@gmail.com>
2022-11-23 22:39:53 +0100

    Adicionando função que adiciona elementos na nossa Dialog, botões. Adicionando também evento de click em cada botão, e mostrando a sua referência no Logcat.

--- app/src/main/java/co/araujoarthur/instagramremake/common/view/DialogCustom.kt
+++ app/src/main/java/co/araujoarthur/instagramremake/common/view/DialogCustom.kt
@@ -3,13 +3,39 @@ package co.araujoarthur.instagramremake.common.view
 import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
 import co.araujoarthur.instagramremake.R
+import org.w3c.dom.Text
 
 class DialogCustom(context: Context): Dialog(context) {
+
+    private lateinit var linearLayout: LinearLayout
+    private lateinit var textViews: Array<TextView>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dialog_custom)
+        linearLayout = findViewById(R.id.dialog_container)
     }
 
+    fun addButton(vararg texts: Int, l: View.OnClickListener) {
 
+        textViews = Array(texts.size) {
+            TextView(context)
+        }
+
+        textViews.forEachIndexed { index, textView ->
+            textViews[index].setText(texts[index])
+            textViews[index].setOnClickListener(l)
+        }
+    }
+
+    override fun show() {
+        super.show()
+        textViews.forEachIndexed { index, textView ->
+            linearLayout.addView(textView)
+        }
+    }
 }
--- app/src/main/java/co/araujoarthur/instagramremake/register/view/RegisterPhotoFragment.kt
+++ app/src/main/java/co/araujoarthur/instagramremake/register/view/RegisterPhotoFragment.kt
@@ -1,9 +1,11 @@
 package co.araujoarthur.instagramremake.register.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import co.araujoarthur.instagramremake.R
 import co.araujoarthur.instagramremake.common.view.DialogCustom
@@ -21,6 +23,11 @@ class RegisterPhotoFragment: Fragment(){
         super.onViewCreated(view, savedInstanceState)
 
         val dialogCustom = DialogCustom(requireContext())
+        dialogCustom.addButton(R.string.photo, R.string.gallery) {
+            Log.i("Test", (it as TextView).text.toString())
+            dialogCustom.dismiss()
+        }
+
         dialogCustom.show()
 
     }
