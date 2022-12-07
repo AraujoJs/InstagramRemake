61493b06600bc38ec09414827ca0f0e82a7abb26
Arthur Lima <mozart.bachh@gmail.com>
2022-12-07 15:31:12 +0100

    Adicionando RecycleView dos posts, da tela de perfil do usuario.

--- app/src/main/java/co/araujoarthur/instagramremake/profile/view/ProfileFragment.kt
+++ app/src/main/java/co/araujoarthur/instagramremake/profile/view/ProfileFragment.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import co.araujoarthur.instagramremake.R
 
-class ProfileFragment: Fragment() {
+class ProfileFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -15,4 +18,37 @@ class ProfileFragment: Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val rv = view.findViewById<RecyclerView>(R.id.rv_profile)
+        rv.layoutManager = GridLayoutManager(requireContext(), 3)
+        rv.adapter = PostAdapter()
+    }
+
+    private class PostAdapter : RecyclerView.Adapter<PostAdapter.PostViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+            return PostViewHolder(
+                LayoutInflater.from(parent.context)
+                    .inflate(R.layout.item_profile_grid, parent, false)
+            )
+        }
+
+        override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+            holder.bind(R.drawable.ic_insta_add)
+        }
+
+        override fun getItemCount(): Int {
+            return 30
+        }
+
+        inner class PostViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            fun bind(imageId: Int) {
+                itemView.findViewById<ImageView>(R.id.item_profile_image_grid)
+                    .setImageResource(imageId)
+
+            }
+        }
+    }
+
 }
